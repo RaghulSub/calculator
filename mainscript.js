49fb80fb3b4b2b1e93d547e9f78c93d8753b6a24
RaghulSub <raghulsubramanian10@gmail.com>
2024-02-02 22:45:32 +0530

    debuged the leading zeros bug in js

--- mainscript.js
+++ mainscript.js
@@ -4,9 +4,11 @@ var expression = ""; // it is the expression to display on output display
 var output = document.getElementById("output"); // it is the search of the output container
 
 function press(value) {
+  // press() function used to handle every number and symbols except equal
+
   // used to parse integer and symbols
-  console.log(value);
   if (value != "=" || value != "") {
+    console.log(expression);
     expression += value;
     output.innerHTML = expression;
   }
@@ -15,26 +17,51 @@ function Clear() {
   //function to clear everything and give a new start
   result = 0;
   expression = "";
-  output.innerHTML = "0";
+  output.innerHTML = result;
 }
 function backspace() {
   //function to clear last element
   expression = expression.slice(0, -1);
-  output.innerHTML = expression;
-  console.log(expression);
+  if (expression.length == 0) {
+    // once the expression's length is reduced to zero everything is clear;
+    expression = "";
+    result = 0;
+    output.innerHTML = result;
+  } else {
+    output.innerHTML = expression;
+  }
 }
+
+function remove_leading_zeros(exp) {
+  for (let i = 0; i < exp.length; i++) {
+    if (exp[i] != "0") {
+      return exp.substring(i);
+    }
+  }
+  return exp;
+}
+
 function expressionEvaluation() {
   // function to evaluate the expression
   if (expression.length == 0) {
-    expression = "0";
+    result = 0;
+    output.innerHTML = result;
   }
+  console.log("Result:" + expression);
 
   try {
     // try to check if any error occurs in evaluation
-    result = eval(expression);
+
+    if (expression.length != 0) {
+      expression = remove_leading_zeros(expression);
+      result = eval(expression);
+    } else {
+      result = 0;
+    }
   } catch (error) {
+    console.log(error);
     output.innerHTML = "Invalid";
   }
   expression = result.toString();
-  output.innerHTML = result;
+  output.innerHTML = expression;
 }
